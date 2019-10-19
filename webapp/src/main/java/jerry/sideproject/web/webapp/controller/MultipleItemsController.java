141668b954ef0bf4f635762b8e77b53ef571e1fd
Jerry <mchen81@dons.usfca.edu>
2019-10-19 16:05:52 -0700

    fix amount bugs

--- webapp/src/main/java/jerry/sideproject/web/webapp/controller/MultipleItemsController.java
+++ webapp/src/main/java/jerry/sideproject/web/webapp/controller/MultipleItemsController.java
@@ -24,7 +24,7 @@ public class MultipleItemsController {
     @GetMapping(value = "/all")
     public String showAll(Model model) {
         model.addAttribute("items", itemService.findAll());
-        model.addAttribute("total", String.format("%2f", itemService.getTotalAmount()));
+        model.addAttribute("total", String.format("%.2f", itemService.getTotalAmount()));
         return "allItems";
     }
 
@@ -35,7 +35,6 @@ public class MultipleItemsController {
             itemForm.addItem(new Item());
         }
         model.addAttribute("form", itemForm);
-        model.addAttribute("total", String.format("%2f", itemService.getTotalAmount()));
         return "createItemsForm";
     }
 
@@ -46,7 +45,6 @@ public class MultipleItemsController {
                 .iterator()
                 .forEachRemaining(items::add);
         model.addAttribute("form", new ItemsCreationDto(items));
-        model.addAttribute("total", String.format("%2f", itemService.getTotalAmount()));
         return "editItemsForm";
     }
 
@@ -54,11 +52,10 @@ public class MultipleItemsController {
     public String saveItems(@ModelAttribute ItemsCreationDto form, Model model) {
         itemService.saveAll(form.getItems());
         model.addAttribute("items", itemService.findAll());
-        model.addAttribute("total", String.format("%2f", itemService.getTotalAmount()));
         return "redirect:/items/all";
     }
 
-    @PostMapping(value = "/submitToDB")
+    @GetMapping(value = "/submitToDB")
     public String submit() {
         return "TODO"; // TODO
     }
@@ -67,7 +64,7 @@ public class MultipleItemsController {
     public String clear(Model model) {
         itemService.clear();
         model.addAttribute("items", itemService.findAll());
-        model.addAttribute("total", String.format("%2f", itemService.getTotalAmount()));
+        model.addAttribute("total", String.format("%.2f", itemService.getTotalAmount()));
         return "redirect:/items/all";
     }
 }
